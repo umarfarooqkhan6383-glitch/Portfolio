cc7fed1476020f25ca1b246fa4ae5578e87338d7
umarfarooqkhan6383-glitch <umarfarooqkhan6383@gmail.com>
2025-11-11 15:28:21 +0500

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Home from './pages/home'
-import About from './pages/about'
-import Projects from './pages/projects'
-import Contact from './pages/contact'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/home';
+import About from './pages/about';
+import Projects from './pages/projects';
+import Contact from './pages/contact';
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -18,8 +18,8 @@ const App = () => {
         <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
-    </Router>
-  )
-}
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { HeroUIProvider } from '@heroui/react'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { HeroUIProvider } from '@heroui/react';
+import { BrowserRouter } from 'react-router-dom'; // ✅ react-router-dom se
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -13,4 +13,4 @@ createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </HeroUIProvider>
   </StrictMode>
-)
+);
