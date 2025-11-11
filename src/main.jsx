4e07cce89e4cd8daf58a0832b662f8126d1053d8
umarfarooqkhan6383-glitch <umarfarooqkhan6383@gmail.com>
2025-11-11 15:16:56 +0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { HeroUIProvider } from '@heroui/react'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HeroUIProvider>
