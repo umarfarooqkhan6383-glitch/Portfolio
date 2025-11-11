7bceab11fdfd54a50184643e1ef90eb2c34faef0
umarfarooqkhan6383-glitch <umarfarooqkhan6383@gmail.com>
2025-11-11 15:12:28 +0500

    add browserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { HeroUIProvider } from '@heroui/react'
+import { BrowserRouter } from 'react-router'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HeroUIProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </HeroUIProvider>
   </StrictMode>
 )
