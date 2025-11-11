838b80bcc2f564f7fa6c18d818996c6706fe2958
umarfarooqkhan6383-glitch <umarfarooqkhan6383@gmail.com>
2025-11-11 14:34:07 +0500

    Changes in Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/home'
 import About from './pages/about'
-import Projects from './pages/projects'
-import Contact from './pages/contact'
+
 
 const App = () => {
   return (
@@ -14,8 +13,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
